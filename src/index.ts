0584d51a3b651910731bb357e3cf560e70c18fbf
Luois45 <github@louis45.de>
2023-07-31 06:53:39 +0200

    Created a working loader for the time being

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import { Injector, webpack } from "replugged";
 
 const injector = new Injector();
+let intervalId: NodeJS.Timeout | null = null;
 
 export async function start(): Promise<void> {
-  // Find the module containing the getCurrentUser function
-  const v = webpack.getByProps("getCurrentUser").getCurrentUser();
+  intervalId = setInterval(() => {
+    const v = webpack.getByProps("getCurrentUser").getCurrentUser();
 
-  if (typeof v === "undefined") return;
+    if (typeof v === "undefined") return;
+    if (v == true) return;
 
-  v.nsfwAllowed = true;
-
-  console.log(v);
+    v.nsfwAllowed = true;
+  }, 1000);
 }
 
 export function stop(): void {
   // Remove all injections when the plugin is stopped
   injector.uninjectAll();
+
+  // Clear the interval when the plugin is stopped
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
 }
